82bc40506d3e4026a418662ee24626e6f24b47fd
Nikola Miladinovic <user6666user7777@gmail.com>
2023-04-30 20:05:24 +0100

    Dodavanja/Izmene

--- MA2023/app/src/main/java/com/example/ma2023/activities/MojBrojActivity.java
+++ MA2023/app/src/main/java/com/example/ma2023/activities/MojBrojActivity.java
@@ -2,8 +2,12 @@ package com.example.ma2023.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.ma2023.MainActivity;
 import com.example.ma2023.R;
 
 public class MojBrojActivity extends AppCompatActivity {
--- MA2023/app/src/main/java/com/example/ma2023/activities/SkockoActivity.java
+++ MA2023/app/src/main/java/com/example/ma2023/activities/SkockoActivity.java
@@ -16,6 +16,55 @@ public class SkockoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skocko);
 
+//        final Button btnSkocko = findViewById(R.id.imageButton7nSkocko);
+//        btnSkocko.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+//        final Button btnHerc = findViewById(R.id.imageButton7nHerc);
+//        btnHerc.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+//        final Button btnKaro = findViewById(R.id.imageButton7nKaro);
+//        btnKaro.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+//        final Button btnZvezda = findViewById(R.id.imageButton7nZvezda);
+//        btnZvezda.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+//        final Button btnPik = findViewById(R.id.imageButton7nPik);
+//        btnPik.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+//        final Button btnTref = findViewById(R.id.imageButton7nTref);
+//        btnTref.setOnClickListener(new ?.OnClickListener() {
+//            public void onClick() {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+
+
 
         final Button btn7n1 = findViewById(R.id.button7n1);
         btn7n1.setOnClickListener(new View.OnClickListener() {
